e6d1f657e17529bb5a83b4b62eb9663c2522fe67
Firzan Armani <mrfirzanarmani@gmail.com>
2022-04-12 18:26:04 +0800

    Setup linting for frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +10,5 @@ ReactDOM.render(
       <App />
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
